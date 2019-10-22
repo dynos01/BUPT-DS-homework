51f1c8a60d6860f245433ca02997f6d2f7349f21
dynos01 <i@dyn.im>
2019-10-22 11:47:25 +0000

    Added homework parantheses check

---
+++ parentheses-check.cpp
@@ -0,0 +1,33 @@
+/*
+ * License: GPLv3
+ * BUPT DS homework: Parentheses Check
+ * Tested under G++ 9.2.0, with minimal parameters
+ * Author: Yinuo Deng <i@dyn.im>
+ * Last modified: Tue 22 Oct 2019 19:31:16 PM CST
+*/
+
+#include <iostream>
+#include <string>
+
+bool match(std::string exp) {
+    int j = 0;
+    std::string pSet = "()[]";
+    char *myStack = new char[exp.length()];
+    for (size_t i = 0; i <= exp.length() && exp.length() > 0; i++) {
+        if (pSet.find(exp[i]) == std::string::npos) continue;
+        myStack[j] = exp[i];
+        if (j == 0) j++;
+        else if (myStack[j - 1] == '(' && myStack[j] == ')') j--;
+        else if (myStack[j - 1] == '[' && myStack[j] == ']') j--;
+        else j++;
+    }
+    delete [] myStack;
+    return j <= 0;
+}
+
+int main(void) {
+    std::string exp;
+    std::getline(std::cin, exp);
+    std::cout << (match(exp) ? "Yes" : "No") << std::endl;
+    return 0;
+}
